758fd25b5bec413c7f1eac60d91a650347fef60d
susila-tropika <susilatropikanew@gmail.com>
2021-08-26 05:57:11 +0700

    merevisi tugas javascript  niomic

--- tugas2.js
+++ tugas2.js
@@ -7,7 +7,8 @@ let bahasaPemrogramman = ['java','php','javascript','C++','python'];
 console.log(`No    : ${number}`);
 console.log(`Judul : ${string}`);
 if (tugasSelesai=true){
-    console.log(`Tugas ${bahasaPemrogramman[2]}sudah diselesaikan`)
+    console.log(`Tugas ${bahasaPemrogramman[2]} sudah diselesaikan`)
 }else{
     console.log("mohon maaf saya belum mengerjakan tugasnya")
 }
+console.log(kosong);
